283da06a53201511b0e20891829ee2d9d6f4a1f7
Aaaabhinav <abhinav6427@gmail.com>
2025-02-10 23:35:15 +0530

    adding inital files

---
+++ array.js
@@ -0,0 +1,10 @@
+const fs =require("fs");
+  
+  fs.readFile("randomdata.json","utf-8",(error,res)=>{
+      if(error){
+          console.log(error);
+      }else{
+          console.log(JSON.parse(res)[1].id);
+         
+      }
+  })
---
+++ obj.js
@@ -0,0 +1,6 @@
+const json_object = {   "name": "John Doe",   "age": 33 }; 
+ console.log(json_object);
+  console.log(JSON.stringify(json_object)); 
+  
+
+  
